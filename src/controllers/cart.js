f9c7087091c773533411ba852a91df867341cfb6
Samiksha <singhsamiksha877@gmail.com>
2025-01-31 23:19:28 +0530

    Added cart add api

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,4 +1,5 @@
 const CartModel = require('../models/cart');
+const ProductModel = require('../models/product');
 const ErrorUtil = require('../utils/error');
 
 const CartController = {};
@@ -56,7 +57,49 @@ CartController.getUserCart = async (req, res) => {
     }
 };
 
-CartController.addUpdateProductQuantityInCart = async (req, res) => {
+CartController.addProductToCart = async (req, res) => {
+    const { productId, quantity } = req.body;
+    const { _id: userId } = req.user;
+
+    try {
+        const product = await ProductModel.findById(productId);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found', re: req.body });
+        }
+
+        const cart = await CartModel.findOne({ userId });
+        if (cart) {
+            const itemIndex = cart.items.findIndex((item) => item.productId.toString() === productId);
+
+            if (itemIndex > -1) {
+                cart.items[itemIndex].quantity += quantity;
+            } else {
+                cart.items.push({
+                    productId,
+                    quantity,
+                    addedOn: new Date(),
+                });
+            }
+            await cart.save();
+        } else {
+            const newCart = new CartModel({
+                userId,
+                items: [{
+                    productId,
+                    quantity,
+                    addedOn: new Date(),
+                }],
+            });
+            await newCart.save();
+        }
+
+        return res.status(200).json({ message: 'Product added to cart' });
+    } catch (e) {
+        return ErrorUtil.APIError(e, res);
+    }
+};
+
+CartController.updateProductQuantityInCart = async (req, res) => {
     const { productId } = req.params;
     const { quantity } = req.body;
     const { _id: userId } = req.user;
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,7 +5,8 @@ const Auth = require('../middlewares/auth');
 const router = express.Router();
 
 router.get('/', Auth.authenticateJWTToken, CartController.getUserCart);
-router.post('/:productId', Auth.authenticateJWTToken, CartController.addUpdateProductQuantityInCart);
+router.post('/', Auth.authenticateJWTToken, CartController.addProductToCart);
+router.put('/:productId', Auth.authenticateJWTToken, CartController.updateProductQuantityInCart);
 router.delete('/:productId', Auth.authenticateJWTToken, CartController.removeItemFromCart);
 
 module.exports = router;
